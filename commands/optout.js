7f9b0ccc18effeab954ae19bbc3b42859343d382
hazre <37149950+hazre@users.noreply.github.com>
2024-11-15 03:27:30 +0100

    resolve merge conflicts

--- commands/optout.js
+++ commands/optout.js
@@ -1,25 +1,29 @@
-import { SlashCommand } from 'slash-create';
-import bot from '../index.js';
+import { SlashCommand } from "slash-create";
+import { OptedOut } from "../index.js";
 
 export default class extends SlashCommand {
   constructor(creator) {
     super(creator, {
-      name: 'optout',
-      description: 'Opt out of automatic song links.',
+      name: "optout",
+      description: "Opt out of automatic song links.",
     });
   }
 
   async run(ctx) {
     await ctx.defer({ ephemeral: true });
 
-    if (bot.optOutDB.fetch((u) => u.user === ctx.user.id)) {
-      bot.optOutDB.remove((u) => u.user === ctx.user.id);
-      ctx.sendFollowUp('You have opted back into automatic song links on your messages globally.\nTo opt back out, just run this command again.');
-      return;
+    let [user, created] = await OptedOut.findOrCreate({
+      where: { UserID: ctx.user.id },
+    });
+    if (created) {
+      ctx.sendFollowUp(
+        "You have opted out of automatic song links on your messages globally.\nTo opt back in, just run this command again."
+      );
+    } else {
+      await OptedOut.destroy({ where: { UserID: user.UserID } });
+      ctx.sendFollowUp(
+        "You have opted back into automatic song links on your messages globally.\nTo opt back out, just run this command again."
+      );
     }
-
-    bot.optOutDB.create({ user: ctx.user.id });
-
-    ctx.sendFollowUp('You have opted out of automatic song links on your messages globally.\nTo opt back in, just run this command again.');
   }
 }
